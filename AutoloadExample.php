fdfc4ce012a2007da5ca7d8d5f9db9d4155c33b6
checnev <checnev1997@yandex.ru>
2019-12-04 10:44:33 +0500

    create

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace koperdog\yii2treeview;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
